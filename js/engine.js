f67e3cddbaefa37304db68690a6d034fc94f7858
lybell <32293736+lybell-art@users.noreply.github.com>
2021-05-15 16:24:14 +0900

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -5,18 +5,40 @@ function cycle(n, p, c)
 	let v=n+p;
 	return v-Math.floor(v/c)*c;
 }
+function flip(x, axis)
+{
+	let dist=x-axis;
+	return axis - dist;
+}
 
 class cubeSpace
 {
 	constructor()
 	{
-		this.size=500;
+		this.width=500;
+		this.height=500;
 		this.level = 0;
 		this.bounding=[];
 		this.face = 0;
 		this.r = 0;
 		this.nextFace= 0;
 		this.rotateDir=0;
+	}
+	get upperBound()
+	{
+		return -this.height /2;
+	}
+	get lowerBound()
+	{
+		return this.height/2;
+	}
+	get leftBound()
+	{
+		return -this.width /2;
+	}
+	get rightBound()
+	{
+		return this.width/2;
 	}
 	rotate(direction)
 	{
@@ -40,7 +62,7 @@ class cubeSpace
 	render()
 	{
 		rotateY(this.r / 180 * PI);
-		box(this.size);
+		box(this.width, this.height, this.width);
 	}
 }
 
@@ -50,24 +72,69 @@ class ballPlayer
 	{
 		this.pos=new p5.Vector();
 		this.dir=new p5.Vector.random2D();
+		this.radius = 5;
 		this.isMoving=true;
 	}
-	checkBound()
+	get gravity()
 	{
+		return 1;
 	}
-	move(isRotating)
+	get x()
+	{
+		return this.pos.x;
+	}
+	get y()
+	{
+		return this.pos.y;
+	}
+	checkBound(prepos, predir, map)
+	{
+		let collided = false;
+		if(this.pos.y - this.radius < map.upperBound)
+		{
+			this.pos.y = flip(this.pos.y, map.upperBound);
+			this.dir.y = Math.abs(this.dir.y);
+			collided = true;
+		}
+		else if(this.pos.y + this.radius > map.lowerBound)
+		{
+			this.pos.y = flip(this.pos.y, map.lowerBound);
+			this.dir.y = -Math.abs(this.dir.y);
+			collided = true;
+		}
+		if(this.pos.x - this.radius < map.leftBound)
+		{
+			this.pos.y = flip(this.pos.x, map.leftBound);
+			this.dir.y = Math.abs(this.dir.x);
+			collided = true;
+		}
+		else if(this.pos.x + this.radius > map.rightBound)
+		{
+			this.pos.x = flip(this.pos.x, map.rightBound);
+			this.dir.x = -Math.abs(this.dir.x);
+			collided = true;
+		}
+		if(collided)
+		{
+			this.dir.mult(0.9);
+		}
+	}
+	move(isRotating, map)
 	{
 		if(this.isMoving)
 		{
+			this.dir.y += this.gravity;
+			let prePos=this.pos.copy();
 			let realDir=this.dir.copy();
 			if(isRotating) realDir.mult(0.1);
 			this.pos.add(realDir);
+			this.checkBound(prePos, realDir, map);
 		}
 	}
 	render()
 	{
 		push();
-		circle(this.pos.x, this.pos.y,10);
+		circle(this.x, this.y, this.radius * 2);
 		pop();
 	}
 }
@@ -86,7 +153,8 @@ function setup()
 	frameRate(60);
 	createCanvas(windowWidth,windowHeight,WEBGL);
 	ortho();
-	world=new cubeSpace(500);
+	world=new cubeSpace();
+	ball=new ballPlayer();
 	fill(255);
 }
 
@@ -100,7 +168,7 @@ function ingame()
 {
 	let isRotating=false;
 	isRotating = world.operate();
-	ball.move(isRotating);
+	ball.move(isRotating, world);
 	world.render();
 	if(isRotating) drawOverlay();
 	ball.render();
